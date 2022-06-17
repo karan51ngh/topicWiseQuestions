d90fdc9d33dcecb05305f4db07401e0449688b23
karan51ngh <karansingh9535@gmail.com>
2022-06-17 12:15:39 +0530

    Removed Redundant files

--- greedy/beautiful_paris.cpp
+++
@@ -1,7 +0,0 @@
-// https://www.hackerrank.com/challenges/beautiful-pairs/problem?isFullScreen=true
-#include <iostream>
-using namespace std;
-int main()
-{
-    int
-}
--- greedy/sherlock_beast.cpp
+++
@@ -1,4 +0,0 @@
-// https://www.hackerrank.com/challenges/sherlock-and-the-beast/problem?isFullScreen=true
-// we need 5x + 3y = n
-// x shd be min,(the slot for 3's)
-// y shd be max,(the slot for 5's)
